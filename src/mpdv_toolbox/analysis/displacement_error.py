6aae00a577c2933c35691362e7fa72c8982d72b8
Lucas Rackers <lracker1@jh.edu>
2026-08-13 14:07:44 -0400

    update displacement error analysis code

--- src/mpdv_toolbox/analysis/displacement_error.py
+++ src/mpdv_toolbox/analysis/displacement_error.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from scipy.stats import norm
 
 
 def signal_to_noise(real, imag, noise_floor):
@@ -14,13 +15,12 @@ def signal_to_noise(real, imag, noise_floor):
 
 def unwrap_phase(real, imag):
     analytical = real + 1j * imag
-    a = analytical[:-1]
-    b = analytical[1:]
-    delta_theta = np.angle(b/a)
-    delta_theta = np.insert(delta_theta,0,0)
-    phas = np.cumsum(delta_theta)
+    theta = np.angle(analytical)
+    delta_theta = np.diff(theta)
+    delta_theta = np.insert(delta_theta, 0 , 1e-10)
+    phas = np.unwrap(theta)
 
-    return phas
+    return theta, delta_theta, phas
 
 def phas_to_pos(time, phas, lam, fit_time):
     mask = time < fit_time
@@ -29,18 +29,32 @@ def phas_to_pos(time, phas, lam, fit_time):
 
     return pos
 
-def phas_uncert(snr):
+def phas_uncert(delta_theta, snr):
     var_theta = 1/snr
     a = var_theta[:-1]
     b = var_theta[1:]
     var_delta_theta = a + b
-    var_delta_theta = np.insert(var_delta_theta, 0 , 0)
+    var_delta_theta = np.insert(var_delta_theta, 0 , 1e-10)
+    
+    p1 = 1 - norm.cdf(np.pi, loc=delta_theta, scale=np.sqrt(var_delta_theta))
+    p2 = norm.cdf(-np.pi, loc=delta_theta, scale=np.sqrt(var_delta_theta))
 
-    var_phas = np.cumsum(var_delta_theta)
+    var_f = p2 * (1 - p2) + p1 * (1 - p1) + 2 * p1 * p2
 
-    sigma_phas = np.sqrt(var_phas)
+    # this assumes independence between each time measurement. TODO: this is not necessarily true
+    var_phas = var_theta + 4 * np.pi**2 * np.cumsum(var_f)
 
-    return sigma_phas
+    return var_delta_theta, var_phas
+
+def displacement_uncert(real, imag, snr):
+    theta, delta_theta, phas = unwrap_phase(real, imag)
+    var_delta_theta, var_phas = phas_uncert(delta_theta, snr)
+    sigma_pos = lam / 2 * np.pi * np.sqrt(var_phas)
+
+    return sigma_pos
+
+
+
 
 if __name__ == "__main__":
     import pandas as pd
@@ -51,77 +65,42 @@ if __name__ == "__main__":
     df = pd.read_csv(fname)
     valid = df[f"probe_{probe_num}_real"].notna()
     time = df["time"][valid].to_numpy()
-    real = df[f"probe_{probe_num}_real"][valid].to_numpy()
-    imag = df[f"probe_{probe_num}_imag"][valid].to_numpy()
 
-    noise_floor = 1e-3  # TODO: incorporate alpss noise frac
+    mask = (time > 750e-9) & (time < 2200e-9)
+
+    time = time[mask]
+    real = df[f"probe_{probe_num}_real"][valid].to_numpy()[mask]
+    imag = df[f"probe_{probe_num}_imag"][valid].to_numpy()[mask]
+
+    noise_floor = 1e-4  # TODO: incorporate alpss noise frac
     fs = 1.0 / (time[1] - time[0])
     lam = 1.55e-6  # from this shot's ALPSS -inputs.csv
 
     uncertainty_multiplier = 100  # exaggerate the uncertainty band so it's visible against the full displacement
 
-    phas = unwrap_phase(real, imag)
-
-    pos = phas_to_pos(time, phas, lam, 500e-9)
+    theta, delta_theta, phas = unwrap_phase(real, imag)
 
     snr = signal_to_noise(real, imag, noise_floor)
 
-    sigma_phas = phas_uncert(snr)
+    var_delta_theta, var_phas = phas_uncert(delta_theta, snr)
 
-    sigma_pos = lam / 2 / np.pi * sigma_phas
+    sigma_pos = lam / 2 * np.pi * np.sqrt(var_phas)
 
-    mask = (time > 750e-9) & (time < 2200e-9)
-    plt.plot(time[mask]*1e9, sigma_pos[mask]*1e6 - sigma_pos[mask][0]*1e6)
+    plt.plot(time*1e9, sigma_pos*1e6)
     plt.xlabel("Time (ns)")
     plt.ylabel("Displacement Uncertainty (microns)")
-    plt.savefig("displacement_uncert.png",dpi=300)
+    plt.savefig("displacement_uncert2.png",dpi=300)
     plt.show()
 
-    plt.plot(time[mask]*1e9, pos[mask]*1e6 - pos[mask][0]*1e6, label='Displacement')
-    plt.fill_between(time[mask]*1e9, pos[mask]*1e6 - pos[mask][0]*1e6 - (sigma_pos[mask]*1e6 - sigma_pos[mask][0]*1e6)/2, pos[mask]*1e6 - pos[mask][0]*1e6 + (sigma_pos[mask]*1e6 - sigma_pos[mask][0]*1e6)/2, color = 'r', alpha=0.5,label='Displacement Uncertainty')
-    plt.xlabel("Time (ns)")
-    plt.ylabel("Displacement (microns)")
-    plt.legend()
-    plt.savefig("displacement_w_uncert.png",dpi=300)
+    idx = 10000
+    
+    dt = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+    time_idx = np.argmin(np.abs(1000e-9 - time))
+    pdf = norm.pdf(dt, loc=delta_theta[time_idx], scale=np.sqrt(1/snr[time_idx]))
+    plt.plot(dt, pdf)
+    # plt.xlim([-2 * np.pi, 2 * np.pi])
     plt.show()
 
-    # mask2 = (time > 700e-9) & (time < 700.2e-9)
-    # plt.scatter(real[mask2] * 1e3, imag[mask2] * 1e3, c=time[mask2]*1e9)
-    # # plt.plot(real[mask2]*1e3, imag[mask2]*1e3,'k--', linewidth=0.5)
-    # plt.xlabel("Real (mV)")
-    # plt.ylabel("Imaginary (mV)")
-    # plt.colorbar(label="Time (ns)")
-    # # plt.savefig("Analytical.png", dpi=300)
-    # plt.show()
-
-    # a = 0.0001
-    # b = 0.1
-    # vf = 100
-    # x = np.linspace(-750, 750, 1000)
-    # z = a * x**2 + b * x
-    # vc = vf / (2 * a * x + b)
-    # plt.ylim([-5000,5000])
-    # plt.scatter(x , vc)
-    # plt.xlabel("X Position (microns)")
-    # plt.ylabel("Closure Velocity (m/s)")
-    # plt.axvline(-b/2/a,0,1,color='k',linestyle='--')
-    # plt.savefig("closure_velocity_2d.png", dpi=300)
-    # plt.show()
-
-    A = np.array([[1,0.1],[0.1,0.5]])*1e-5
-    B = np.array([1,0.5])*5e-3
-    xv, yv = np.meshgrid(np.linspace(-750, 750, 100), np.linspace(-750, 750, 100))
-    x = np.vstack((np.reshape(xv, xv.size), np.reshape(yv, xv.size))).T
-    z = np.sum((x @ A) * x, axis=1) + x @ B
-    zv = np.reshape(z, (int(np.sqrt(z.size)),int(np.sqrt(z.size))))
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(xv, yv, zv, cmap="coolwarm",
-                        linewidth=0, antialiased=False)
-    plt.xlabel("x (microns)")
-    plt.ylabel("y (microns)")
-    ax.set_zlabel("z (microns)")
-    plt.savefig("3d_surface.png", dpi=300)
-    plt.show()
-
+    plt.plot()
 
 
